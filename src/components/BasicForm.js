72dc19d9dcdbcac4f45f3f477a6e477c088ebfc4
Irene Riportella <riportellairene@gmail.com>
2022-10-16 16:50:24 +0200

    managed form validation with useReducer()

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -4,99 +4,92 @@ const isNotEmpty = (value) => value.trim() !== '';
 const isEmail = (value) => value.trim().includes('@');
 
 const BasicForm = (props) => {
-    const {
-        value: firstNameValue,
-        isValid: firsNameIsValid,
-        hasError: firstNameHasError,
-        valueChangeHandler: firstNameChangeHandler,
-        inputBlurHandler: firstNameBlurHandler,
-        reset: resetFirstName,
-    } = useInput(isNotEmpty);
-    const {
-        value: lastNameValue,
-        isValid: lastNameIsValid,
-        hasError: lastNameHasError,
-        valueChangeHandler: lastNameChangeHandler,
-        inputBlurHandler: lastNameBlurHandler,
-        reset: resetLastName,
-    } = useInput(isNotEmpty);
-    const {
-        value: emailValue,
-        isValid: emailIsValid,
-        hasError: emailHasError,
-        valueChangeHandler: emailChangeHandler,
-        inputBlurHandler: emailBlurHandler,
-        reset: resetEmail
-    } = useInput(isEmail);
+  const {
+    value: firstNameValue,
+    isValid: firsNameIsValid,
+    hasError: firstNameHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+  } = useInput(isNotEmpty);
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+  } = useInput(isNotEmpty);
+  const { value: emailValue, isValid: emailIsValid, hasError: emailHasError, valueChangeHandler: emailChangeHandler, inputBlurHandler: emailBlurHandler, reset: resetEmail } = useInput(isEmail);
 
-    let formIsValid = false;
+  let formIsValid = false;
 
-    if (firsNameIsValid && lastNameIsValid && emailIsValid) {
-        formIsValid = true;
+  if (firsNameIsValid && lastNameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
     }
 
-    const submitHandler = (event) => {
-        event.preventDefault();
+    console.log('Submitted!');
+    console.log(firstNameValue, lastNameValue, emailValue);
 
-        if (!formIsValid) {
-            return;
-        }
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+  };
 
-        console.log('Submitted!');
-        console.log(firstNameValue, lastNameValue, emailValue);
+  const firstNameClasses = firstNameHasError ? 'form-control invalid' : 'form-control';
+  const lastNameClasses = lastNameHasError ? 'form-control invalid' : 'form-control';
+  const emailClasses = emailHasError ? 'form-control invalid' : 'form-control';
 
-        resetFirstName();
-        resetLastName();
-        resetEmail();
-    };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="control-group">
+        <div className={firstNameClasses}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            value={firstNameValue}
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+          />
+          {firstNameHasError && <p className="error-text">Please enter a first name.</p>}
+        </div>
 
-    const firstNameClasses = firstNameHasError ? 'form-control invalid' : 'form-control';
-    const lastNameClasses = lastNameHasError ? 'form-control invalid' : 'form-control';
-    const emailClasses = emailHasError ? 'form-control invalid' : 'form-control';
-
-    return (
-        <form onSubmit={submitHandler}>
-            <div className="control-group">
-                <div className={firstNameClasses}>
-                    <label htmlFor="name">First Name</label>
-                    <input
-                        type="text"
-                        id="name"
-                        value={firstNameValue}
-                        onChange={firstNameChangeHandler}
-                        onBlur={firstNameBlurHandler}
-                    />
-                    {firstNameHasError && <p className="error-text">Please enter a first name.</p>}
-                </div>
-
-                <div className={lastNameClasses}>
-                    <label htmlFor="name">Last Name</label>
-                    <input
-                        type="text"
-                        id="name"
-                        value={lastNameValue}
-                        onChange={lastNameChangeHandler}
-                        onBlur={lastNameBlurHandler}
-                    />
-                    {lastNameHasError && <p className="error-text">Please enter a last name.</p>}
-                </div>
-            </div>
-            <div className={emailClasses}>
-                <label htmlFor="name">E-Mail Address</label>
-                <input
-                    type="text"
-                    id="name"
-                    value={emailValue}
-                    onChange={emailChangeHandler}
-                    onBlur={emailBlurHandler}
-                />
-                {emailHasError && <p className="error-text">Please enter a valid email.</p>}
-            </div>
-            <div className="form-actions">
-                <button disabled={!formIsValid}>Submit</button>
-            </div>
-        </form>
-    );
+        <div className={lastNameClasses}>
+          <label htmlFor="name">Last Name</label>
+          <input
+            type="text"
+            id="name"
+            value={lastNameValue}
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+          />
+          {lastNameHasError && <p className="error-text">Please enter a last name.</p>}
+        </div>
+      </div>
+      <div className={emailClasses}>
+        <label htmlFor="name">E-Mail Address</label>
+        <input
+          type="text"
+          id="name"
+          value={emailValue}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+        />
+        {emailHasError && <p className="error-text">Please enter a valid email.</p>}
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
 };
 
 export default BasicForm;
--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,27 +1,52 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const initialInputState = {
+  value: '',
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === 'INPUT') {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === 'BLUR') {
+    return { value: state.value, isTouched: true };
+  }
+  if (action.type === 'RESET') {
+    return { value: '', isTouched: false };
+  }
+  return initialInputState;
+};
 
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [isTouched, setIsTouched] = useState(false);
+  const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
+  // const [enteredValue, setEnteredValue] = useState('');
+  // const [isTouched, setIsTouched] = useState(false);
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  // const valueIsValid = validateValue(enteredValue);
+  const valueIsValid = validateValue(inputState.value);
+  // const hasError = !valueIsValid && isTouched;
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    dispatch({ type: 'INPUT', value: event.target.value });
+    // setEnteredValue(event.target.value);
   };
 
   const inputBlurHandler = (event) => {
-    setIsTouched(true);
+    dispatch({ type: 'BLUR' });
+    // setIsTouched(true);
   };
 
   const reset = () => {
-    setEnteredValue('');
-    setIsTouched(false);
+    dispatch({ type: 'RESET' });
+    // setEnteredValue('');
+    // setIsTouched(false);
   };
 
   return {
-    value: enteredValue,
+    // value: enteredValue,
+    value: inputState.value,
     isValid: valueIsValid,
     hasError,
     valueChangeHandler,
